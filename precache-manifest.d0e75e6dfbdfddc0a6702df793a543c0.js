b96c6b6b4025eff2c828b34fc74fbed7da9cbe11
hirenballewar <33598588+hirenballewar@users.noreply.github.com>
2019-07-19 10:51:12 +0100

    Updates

---
+++ precache-manifest.d0e75e6dfbdfddc0a6702df793a543c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "061672f06c3e4b960384e03fa10b31b7",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "6bf479b416231b65581e",
+    "url": "/recipe-app/static/css/main.dbff14c2.chunk.css"
+  },
+  {
+    "revision": "e269caec8435623e6b29",
+    "url": "/recipe-app/static/js/2.881939cd.chunk.js"
+  },
+  {
+    "revision": "6bf479b416231b65581e",
+    "url": "/recipe-app/static/js/main.916bbac3.chunk.js"
+  },
+  {
+    "revision": "82c36fb6d19137cb8b75",
+    "url": "/recipe-app/static/js/runtime~main.11ea1cab.js"
+  }
+]);
